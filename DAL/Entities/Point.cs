d2d00c8f362d109c9585be3e1c32337ab272d19c
DDasha8 <dashka140503@gmail.com>
2021-05-11 18:17:20 +0300

    Добавлены классы сущностей в DAL

---
+++ DAL/Entities/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities
+{
+    public class Point
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int HospitalId {get;set;}
+        public virtual Hospital Hospital { get; set; }
+
+        public virtual List<Journal> Records { get; set; }
+    }
+}
